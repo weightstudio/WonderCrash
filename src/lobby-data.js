1dfeb148b2e3a3067b7f2a0e2d561572c4d23690
DESKTOP-FGA5CDP\User <a032332852@gmail.com>
2026-07-02 09:03:29 +0800

    Replace weak cover art and remove CSS-drawn zoo art

--- src/lobby-data.js
+++ src/lobby-data.js
@@ -237,7 +237,7 @@
       },
       art: {
         kind: "image",
-        background: "assets/garden-tiles-cover.svg",
+        background: "assets/garden-tiles-cover.png",
         hero: "data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='1' height='1'></svg>",
       },
     },
@@ -287,7 +287,7 @@
       },
       art: {
         kind: "image",
-        background: "assets/animal-hidden-safari-cover.svg",
+        background: "assets/animal-hidden-safari-cover.png",
         hero: "data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='1' height='1'></svg>",
       },
     },
